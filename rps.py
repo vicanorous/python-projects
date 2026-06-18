ecbd726971c88029ff50d736cfee40a63446d112
Victor <anotheranonguyy@gmail.com>
2026-06-18 04:30:02 -0700

    fixed basic level games

--- rps.py
+++ rps.py
@@ -1,11 +1,28 @@
-userinput = input("Choose: Rock, Paper, or Scissor (r/p/s): ")
-rock = "r"
-paper = "p"
-scissor = "s"
+import random
 
-while True:
-    print(userinput)
-    if userinput != rock and userinput != scissor and userinput != paper:
-        print("Invalid response, input 'r', or 'p', or 's'")
-        break
-        
+inputchoices = {
+    "r": "Rock",
+    "p": "Paper",
+    "s": "Scissor"
+}
+
+userinput = input("Choose: Rock, Paper, or Scissor (r/p/s): ").lower()
+choices = ("r", "p", "s")
+computerinput = random.choice(choices)
+
+
+print(userinput)
+if userinput not in (choices):
+    print("Invalid response, input 'r', or 'p', or 's'")
+    
+else:
+    print("You chose: " + userinput)
+    print("Computer chose: " + computerinput)
+
+if userinput == computerinput:
+    print("It's a tie")
+elif (userinput == "r" and computerinput == "s") or (userinput == "p" and computerinput == "r") or (userinput == "s" and computerinput == "p"):
+    print("You win")
+
+else:
+    print("You lose")
